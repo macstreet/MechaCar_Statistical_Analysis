8dac61ce05f141a672b2fc3a1a9d21e45ec505b4
macstreet <macstreet23@gmail.com>
2023-02-15 14:46:43 -0700

    README & Add Image 1

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,11 +4,11 @@
 library(dplyr)
 
 #Import/Read mpg CSV file as DataFrame
-MechaCar <- read.csv('MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+MechaCar <- read.csv('resources/Data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
 #Perform linear regression
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
 
-#Determine p-value and r-squared value
+#Determine p-value and r-squared value via summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
 
